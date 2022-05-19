5b292a260cff2027cb928705cc7b8f231f4d1781
Berkant Cetin <b.cetin.berlin@icloud.com>
2022-05-19 09:34:48 +0200

    Searchbar added
    - Sucheingaben erhalten und Ergebnisse anzeigen

--- themes/TaskitTheme/assets/search.js
+++ themes/TaskitTheme/assets/search.js
@@ -1,3 +1,8 @@
+import Fuse from 'fuse.js'
+// erstellt eine Modulvariable zur Speicherung des in allen Funktionen zu verwendenden Index
+let index = null;
+const MAX_SEARCH_RESULTS = 5;
+
 export default {
     async init() {
       try {
@@ -9,10 +14,52 @@ export default {
         }
         // ruft die response-Daten von JSON als ein Objekt ab
         let data = await response.json();
-        // nur zur Fehlersuche
-        console.log(data);
+        // erzeugt einen Fuse.js-Index
+        index = new Fuse(data, {
+          keys: [{
+            // fügt Titel mit einem Gewicht von 20 hinzu
+            name: 'title',
+            weight: 20
+          }, {
+            name: 'tag',
+            weight: 5
+          }, {
+            // wenn nicht angegeben, wird das Gewicht als 1 behandelt.
+            name: 'content'
+          }]
+        });
+        // fügt das Input-Ereignis hinzu
+        document.addEventListener("input", this.showResults);
       } catch (e) {
         this.removeSearch();
+      }
+    },
+
+    showResults(event) {
+      const searchBox = document.querySelector("#search input");
+      if (event.target !== searchBox) {
+        return;
+      }
+      const result = document.querySelector("#search div");
+      if (searchBox.value.length > 0) {
+        const results = index.search(searchBox.value);
+
+        // innerHTML ersetzt den Inhalt der Auswahlliste. 
+        // Wir können stattdessen vorhandene DOM-Elemente aktualisieren, 
+        // wenn die Leistung ein Aspekt ist
+        result.innerHTML = results
+           // begrenzt die Anzahl der Suchergebnisse auf MAX_SEARCH_RESULTS
+          .slice(0, MAX_SEARCH_RESULTS)
+          // Bietet ein umfangreiches Dropdown-Menü mit einem Bild und begleitendem Text
+          .map(x => `<a href="${x.item.url}">
+            <img src="${x.item.cover || ""}" width="40" height="40">
+            <h3>${x.item.title}</h3>
+            <span>${x.item.content.substr(0,40)}</span>
+          </a>`)
+          .join("");
+      } else {
+        
+        result.innerHTML = '';
       }
     },
   
