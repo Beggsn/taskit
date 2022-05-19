e7246baf9984a5287f6c63ef32d35a79e1429cbb
Berkant Cetin <b.cetin.berlin@icloud.com>
2022-05-19 17:08:10 +0200

    Adding LaTex rendering

---
+++ api.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const querystring = require('querystring');
+const latex2svg = require('./api/latex2svg');
+
+// Fragt nach dem Port aus den Umgebungsvariablen oder gibt den Standardwert 3000 an
+const port = process.env.PORT || 3000;
+const server = http.createServer().listen(port);
+
+// richtet einen HTTP-Server ein
+server.on('request', async function (req, res) {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const queryStringParameters = url.search && querystring.parse(url.search.slice(1));
+    // Erzeugt ein mit AWS Lambda kompatibles Request-Objekt
+    const request = {
+      queryStringParameters,
+      path: url.pathname,
+      httpMethod: req.method,
+      headers: req.headers,
+      body: req.body
+    }
+    // erzeugt eine Default Antwort
+    let response = {
+      statusCode: 404,
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({error: "Die Seite wurde nicht gefunden"})
+    }
+
+    // erzeugt einen schlanken Router
+    try {
+      switch (url.pathname) {
+        case '/latex2svg':
+          response = await latex2svg.handler(request);
+          break;
+      }
+    //   ordentlicher Umgang mit Exceptions
+    } catch(e) {
+      response.statusCode = 500;
+      console.log(e);
+      response.body = JSON.stringify(e);
+    }
+
+    response.headers = response.headers || {};
+    // Ermöglicht einen herkunftsübergreifenden Zugang, um zu verhindern, 
+    // dass Heroku von unserer Website aus blockiert wird
+  response.headers['Access-Control-Allow-Origin'] = '*';
+    //   gibt die Antwort an den Kunden zurück
+  res.writeHead(response.statusCode, response.headers);
+    res.end(response.body);
+});
+
+console.log(`Abhören des Ports ${port}`);
---
+++ api/latex2svg.js
@@ -0,0 +1,61 @@
+// LaTeX in SVG umwandeln 
+// importiert die MathJax Abhängigkeit
+const MathjaxModule = require("mathjax");
+
+let MathJax = null;
+module.exports = {
+  /**
+   * Funktion zur Bearbeitung von Aufrufen an den API-Endpunkt der Cloud-Funktion.
+   */
+  async handler(event, context) {
+    if (
+      !event.queryStringParameters ||
+      // verbietet ein leeres LATEX2SVG_PASSWORT
+      !process.env.LATEX2SVG_PASSWORD ||
+      event.queryStringParameters.password !== process.env.LATEX2SVG_PASSWORD
+    ) {
+      return {
+          // Verwendet HTTP 401, wenn das Kennwort falsch ist. 
+          // Wenn das Kennwort korrekt ist und der Benutzer immer noch keinen Zugang hat,
+          // wäre HTTP 403 der richtige Fehlercode.
+        statusCode: 401,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          error: "Zugang verweigert.",
+        }),
+      };
+    }
+    // stellt sicher, dass der Parameter tex verfügbar ist
+    if (!event.queryStringParameters || !event.queryStringParameters.tex) {
+      return {
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          error: "Der erforderliche Parameter `tex` wurde nicht geliefert.",
+        }),
+      };
+    }
+    // initialisiert MathJax nur bei Bedarf. Es nimmt die Eingabe als LaTeX und gibt SVG aus.
+    if (!MathJax) {
+      MathJax = await MathjaxModule.init({
+        loader: { load: ["input/tex", "output/svg"] },
+      });
+    }
+    // übergibt alle Parameter aus dem Query-String an MathJax
+    const svg = MathJax.tex2svg(event.queryStringParameters.tex, {
+      display: event.queryStringParameters.display
+    });
+
+    // gibt den Statuscode 200 mit den Ergebnissen als JSON zurück
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        data: MathJax.startup.adaptor.outerHTML(svg),
+      }),
+    };
+  },
+};
+
+// Wir können diesen Code in einer FaaS-Lösung wie AWS Lambda oder Netlify Functions bereitstellen. 
+// Für PaaS-Lösungen ist ein wenig mehr Arbeit erforderlich.
